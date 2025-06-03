65fb627d09c051d837b62925d4f7cc0023653a60
Neo <431738916@qq.com>
2025-06-03 22:37:29 +0800

    first commit

---
+++ 4.0/springboot4-api-demo/src/main/java/chat/it666/springboot4apidemo/Springboot4ApiDemoApplication.java
@@ -0,0 +1,13 @@
+package chat.it666.springboot4apidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot4ApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot4ApiDemoApplication.class, args);
+    }
+
+}
